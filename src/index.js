08e92e661e6d2429724677a51a6e2f388c302cb8
Jaichandra <jaichandra7@github.com>
2022-06-04 01:59:28 +0530

    api assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jaichandra:9908078754b@cluster0.pwobr.mongodb.net/jaichandra7-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,64 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.get("/movies", function(req,res){
+    let movies =[
+        "Rang de Basanti",
+        "The Shining",
+        "Lord of the rings",
+        "Batman begins",
+    ];
+    
+    res.send(movies);
+});
+
+router.get("/movies/:indexNumber", function (req,res){
+    let movies =[
+        "Rang de Basanti",
+        "The Shining",
+        "Lord of the rings",
+        "Batman begins",
+    ];
+
+    let index =req.params.indexNumber;
+    if (!movies[index]){
+        res.send("please enter valid index number");
+    }
+    res.send(movies[index]);
+});
+
+
+router.get("/films", function(req,res){
+    let films = [
+        { id : 1, name: "The Shining" },
+        { id : 2, name: "Incendies" },
+        { id : 3, name: "Rang de Basanti" },
+        { id : 4, name: "Finding Nemo" },
+    ]
+    res.send(films);
+});
+
+
+
+router.get("/films/:filmid",function(req,res){
+    let films = [
+        {id: 1, name:"The Shining"},
+        {id: 2, name:"Incendies"},
+        {id: 3, name:"Rang de Basanti"},
+        {id: 4, name:"Finding Nemo"},
+    ];
+    const input = req.params.filmid;
+    const result = films.find((ele) => ele.id == input );
+    if (result === undefined){
+        res.send("No movie exists with this id")
+    }
+    res.send(result);
+})
+
+
+
+
+
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
