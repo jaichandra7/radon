4d392668662d4d67374946aa7d3ab3250c72b142
Jaichandra <jaichandra7@github.com>
2022-06-07 01:46:42 +0530

    monogo day 0

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+// const Bookmodel = require("../models/userModel.js")
+const BookController = require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+router.get("/books", function(req,res){
+    res.send(" The great Books ")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/bookSchema", BookController.createBook)
 
+router.get("/listbooks", BookController.getBooksData)
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+ 
 module.exports = router;
